d962af2b15a5d308787b4d2d7a218a4d7ee27fa5
leightonza <131296862+leightonza@users.noreply.github.com>
2025-06-13 09:33:21 +0200

    index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(express.json());
 
 app.get('/webhook', (req, res) => {
   const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+
   const mode = req.query['hub.mode'];
   const token = req.query['hub.verify_token'];
   const challenge = req.query['hub.challenge'];
